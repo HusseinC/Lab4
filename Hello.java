4a6fad8295ea332976325823d1438e44da4f644c
Hussein Chamas <hchamas@umich.edu>
2024-02-08 14:34:21 -0500

    Made changes on new branch

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for(int i=1; i<=10; i++) {
-            System.out.println("Lab 4 project"+i);
+            System.out.println("Lab 4 project new"+i);
         }
     }
 }
